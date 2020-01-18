61fdf94ec897239d2d951bfa82a41473dc116442
David Gond <david.gond.coding@gmail.com>
2020-01-18 01:08:54 +0100

    feat(Scripts/Player): Added movement management

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Temp
+    public int playerNumber;
+
+    public int movespeed;
+
     public int maxShield;
     [SerializeField]
     private int shield;
@@ -36,16 +41,60 @@ public class Player : MonoBehaviour
         }
     }
 
+    private string horizontal;
+    private string vertical;
+
+    private float horizontalDirection;
+    private float verticalDirection;
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
         Shield = maxShield;
         Life = maxLife;
+        SetPlayer(playerNumber);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalDirection = Input.GetAxis("Player"+playerNumber+"_Horizontal");
+        verticalDirection = Input.GetAxis("Player"+playerNumber+"_Vertical");
+
+        Move(horizontalDirection, verticalDirection);
+    }
+
+    private void Move(float hDirection, float vDirection)
+    {
+        Vector3 verticalVector = new Vector3(.0f, 1f, .0f);
+        Vector3 horizontalVector = new Vector3(1f, .0f, .0f);
+        if (vDirection != 0) // Moving up/down
+        {
+            this.transform.Translate(verticalVector * movespeed * Time.deltaTime);
+            Debug.Log("Player " + playerNumber + " is moving " + (vDirection < 0 ? "down" : "up"));
+        }
+        if (hDirection != 0) // Moving left/right
+        {
+            this.transform.Translate(horizontalVector * movespeed * Time.deltaTime);
+            Debug.Log("Player " + playerNumber + " is moving " + (hDirection < 0 ? "left" : "right"));
+        }
+    }
+    public void SetPlayer(int number)
+    {
+        switch (number)
+        {
+            case 1:
+                this.horizontal = "Player1";
+                this.vertical = "Player1";
+                break;
+            case 2:
+                this.horizontal = "Player2";
+                this.vertical = "Player2";
+                break;
+            default:
+                return;
+        }
     }
 }
