d6a27e29bcffe7bc15a8105380da1a18a8a5b1e0
David Gond <david.gond.coding@gmail.com>
2020-01-19 01:09:16 +0100

    feat(Scripts/Hack): Added Hacking game objects

---
+++ Assets/Scripts/HackController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HackController : MonoBehaviour
+{
+    public GameObject hackable;
+
+    private bool isHackable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isHackable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Hack available");
+            isHackable = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Hack not available");
+            isHackable = false;
+        }
+    }
+}
