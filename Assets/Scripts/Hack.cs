e66568a8aee07c6a9d31b46986e83e46ce879508
David Gond <david.gond.coding@gmail.com>
2020-01-19 09:08:01 +0100

    feat(Scripts/Hack): Completed Hack skill, can now destroy objects

---
+++ Assets/Scripts/Hack.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hack : Skill
+{
+    private Infiltrator character;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        character = GetComponent<Infiltrator>();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public override bool Activate()
+    {
+        Debug.Log("Hack");
+        if (character.HackableObject != null)
+        {
+            Debug.Log("Hackable");
+
+            if (base.Activate())
+            {
+                Debug.Log("Hack complete");
+
+                HackController hackController = character.HackableObject.GetComponent<HackController>();
+                Destroy(hackController.hackable);
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- Assets/Scripts/Infiltrator.cs
+++ Assets/Scripts/Infiltrator.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Infiltrator : Character
 {
+    public GameObject HackableObject
+    {
+        get;
+        private set;
+    }
+
     // Start is called before the first frame update
     protected override void Start()
     {
@@ -14,5 +20,26 @@ public class Infiltrator : Character
     protected override void Update()
     {
         base.Update();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision with " + collision.gameObject.tag);
+        switch (collision.gameObject.tag)
+        {
+            case "Hackable":
+                HackableObject = collision.gameObject;
+                break;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Hackable":
+                HackableObject = null;
+                break;
+        }
     }
 }
